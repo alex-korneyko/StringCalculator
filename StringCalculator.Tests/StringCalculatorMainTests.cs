1fd7477cb6e94b78a473faec0bec8ccbdca34aad
alex <a.korneyko@gmail.com>
2022-02-28 19:33:03 +0200

    Init

---
+++ StringCalculator.Tests/StringCalculatorMainTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class StringCalculatorMainTests
+    {
+        [Fact]
+        public void AddTest()
+        {
+        }
+    }
+}
