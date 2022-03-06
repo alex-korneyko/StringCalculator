3e3b3b3c1af49cc7cf14aecb8adb1c0a4751e14e
Alex Korneiko <a.korneyko@gmail.com>
2022-03-06 11:43:26 +0200

    UI-controller test

--- Presentation.Tests/AppTests.cs
+++ Presentation.Tests/AppTests.cs
@@ -10,10 +10,25 @@ namespace Presentation.Tests
         public void Start_ExecutionAndExit()
         {
             var mock = new Mock<IController<string>>();
-            mock.Setup(dataHandler => dataHandler.GetData("Enter comma separated numbers (enter to exit): ")).Returns("5,6");
-            var app = new App(new Calculator(), mock.Object);
+
+            mock.Setup(controller => controller.GetData("Enter comma separated numbers (enter to exit): "))
+                .Returns("5,6");
+
+            mock.SetupSequence(controller => controller.GetData("you can enter other numbers (enter to exit)? "))
+                .Returns("7,8")
+                .Returns(@"//[x][%]\n7x8%9")
+                .Returns("");
             
+            var app = new App(new Calculator(), mock.Object);
             app.Start();
+
+            mock.Verify(a => a.GetData("Enter comma separated numbers (enter to exit): "), Times.Once);
+            mock.Verify(controller => controller.ShowData("Result is: 11"));
+            mock.Verify(a => a.GetData("you can enter other numbers (enter to exit)? "), Times.Exactly(3));
+            mock.Verify(controller => controller.ShowData("Result is: 15"));
+            mock.Verify(a => a.GetData("you can enter other numbers (enter to exit)? "), Times.Exactly(3));
+            mock.Verify(controller => controller.ShowData("Result is: 24"));
+            mock.Verify(a => a.GetData("you can enter other numbers (enter to exit)? "), Times.Exactly(3));
         }
     }
 }
