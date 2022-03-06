233233a62830b7a8aeafb844fd2a96e1d9780bb2
Alex Korneiko <a.korneyko@gmail.com>
2022-03-06 12:29:25 +0200

    AppTests.cs minor refactoring

--- Presentation.Tests/AppTests.cs
+++ Presentation.Tests/AppTests.cs
@@ -9,26 +9,24 @@ namespace Presentation.Tests
         [Fact]
         public void Start_ExecutionAndExit()
         {
-            var mock = new Mock<IController<string>>();
+            var controllerMock = new Mock<IController<string>>();
 
-            mock.Setup(controller => controller.GetData("Enter comma separated numbers (enter to exit): "))
+            controllerMock.Setup(controller => controller.GetData("Enter comma separated numbers (enter to exit): "))
                 .Returns("5,6");
 
-            mock.SetupSequence(controller => controller.GetData("you can enter other numbers (enter to exit)? "))
+            controllerMock.SetupSequence(controller => controller.GetData("you can enter other numbers (enter to exit)? "))
                 .Returns("7,8")
                 .Returns(@"//[x][%]\n7x8%9")
                 .Returns("");
             
-            var app = new App(new Calculator(), mock.Object);
+            var app = new App(new Calculator(), controllerMock.Object);
             app.Start();
 
-            mock.Verify(a => a.GetData("Enter comma separated numbers (enter to exit): "), Times.Once);
-            mock.Verify(controller => controller.ShowData("Result is: 11"));
-            mock.Verify(a => a.GetData("you can enter other numbers (enter to exit)? "), Times.Exactly(3));
-            mock.Verify(controller => controller.ShowData("Result is: 15"));
-            mock.Verify(a => a.GetData("you can enter other numbers (enter to exit)? "), Times.Exactly(3));
-            mock.Verify(controller => controller.ShowData("Result is: 24"));
-            mock.Verify(a => a.GetData("you can enter other numbers (enter to exit)? "), Times.Exactly(3));
+            controllerMock.Verify(controller => controller.GetData("Enter comma separated numbers (enter to exit): "), Times.Once);
+            controllerMock.Verify(controller => controller.GetData("you can enter other numbers (enter to exit)? "), Times.Exactly(3));
+            controllerMock.Verify(controller => controller.ShowData("Result is: 11"));
+            controllerMock.Verify(controller => controller.ShowData("Result is: 15"));
+            controllerMock.Verify(controller => controller.ShowData("Result is: 24"));
         }
     }
 }
